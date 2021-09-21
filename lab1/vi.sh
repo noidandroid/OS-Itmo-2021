371531f867998bbaa4934f1d39db441988d53a9f
Danya <48490323+noidandroid@users.noreply.github.com>
2021-09-21 22:46:10 +0300

    Update vi.sh

--- lab1/vi.sh
+++ lab1/vi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILE="/var/log/Xorg.0.log"
+FILE="/var/log/anaconda/X.log"
 
 sed -n '/error/p "$FILE" >full.log
 sed '/error/d' "$FILE" >>full.log
 sed 's/warn/Warning:/g' full.log >"curr"
-sed 's/info/Information:/g' "curr" >full.log
+sed 's/info/Information:/g' "curr" >full.log"
 rm curr
