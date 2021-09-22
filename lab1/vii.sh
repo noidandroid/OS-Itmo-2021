4a89871f64cb1eb7f185469d296157fd1d099745
Danya <48490323+noidandroid@users.noreply.github.com>
2021-09-22 16:46:40 +0300

    Update vii.sh

--- lab1/vii.sh
+++ lab1/vii.sh
@@ -2,7 +2,7 @@
 
 IFS=$'\n'
 
-for email in $(grep -Eiorh "[0-9A-Za-z_.]+@[0-9A-Za-z]+(\.[0-9A-Za-z]+)+" /etc)
+for email in $(grep -E -i -o -r -h -s "[0-9A-Za-z_.]+@[0-9A-Za-z]+(\.[0-9A-Za-z]+)+" /etc)
 do
   echo -n $email", "
 done > emails.lst
