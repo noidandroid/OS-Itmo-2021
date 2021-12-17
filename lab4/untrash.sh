961983d79f60fad3ebfc98755d8c7682a68ab9d3
Danya <48490323+noidandroid@users.noreply.github.com>
2021-12-17 11:52:04 +0300

    Update untrash.sh

--- lab4/untrash.sh
+++ lab4/untrash.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-TRASH_DIR=../.trash
-TRASH_LOG=../.trash.log
+dir=../.trash
+log=../.trash.log
+
+if [[ $# != 1 ]];
+  then
+      echo "Arguments exception: need 1 argument to be passed"
+      exit 1
+  fi
+
 
 restore_file () {
     path=$1
     filename=$2
-    ln "$TRASH_DIR/$filename" $path
+    ln -- $dir/"$filename" "$path"
 }
 
 
 filename=$1
 line=""
-grep $filename $TRASH_LOG |
+grep -- "$filename" $log |
 while read line; do
-    file_to_restore=$(echo $line | cut -d " " -f 1)
-    file_in_trash=$(echo $line | cut -d " " -f 2)
-    echo $file_in_trash
+    fullpath=$(echo "$line" | awk -F"'" '{print $1}')
+    fileRestore=$(dirname "$fullpath")
+    fileTrash=$(echo "$line" | awk '{print $NF}')
 
-    echo "Restore $file_to_restore ? (y/n)"
+    echo "Want to restore? (y/n)"
     read answer < /dev/tty
-    if [[ "$answer" == "y" ]]; then
-        parent_directory=$(dirname "$file_to_restore") &&
+    case  "$answer" in
+    "y")
+        parent_directory="$fileRestore" &&
             if [[ -d "$parent_directory" ]]; then
-                $(restore_file "$file_to_restore" "$file_in_trash")
+                $(restore_file "$fileRestore" "$fileTrash")
             else
-                $(restore_file "$HOME/$filename" "$file_in_trash") &&
-                    echo "Directory $parent_directory not exists anymore, restoring at $HOME"
+                $(restore_file "$HOME/$filename" "$fileTrash") &&
+                echo "Directory $parent_directory not exists anymore, restoring at $HOME"
             fi &&
-            rm $TRASH_DIR/$file_in_trash && {
-                sed -i "#$line#d" $TRASH_LOG
-                echo "Restored $file_to_restore"
-            }
-    fi
+            rm -- $dir/"$fileTrash" && echo "$filename was successfully restored"
+	;;
+     "n")
+        ;;
+      *)
+       echo "INVALID COMMAND"
+       ;;
+ esac
 done
