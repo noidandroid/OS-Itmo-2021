f61a5ea8958a89870d6a8dc056098feaaadc1c35
Danya <48490323+noidandroid@users.noreply.github.com>
2021-12-17 11:51:16 +0300

    Update rmtrash.sh

--- lab4/rmtrash.sh
+++ lab4/rmtrash.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-TRASH_DIR=../.trash
-TRASH_LOG=../.trash.log
-
+trash=~/.trash
+trashlog=~/.trash.log
 filename=$1
+version=0
 
-if [[ ! -d "$TRASH_DIR" ]]; then
-    mkdir $TRASH_DIR
+if [ $# != 1 ]; then
+    echo "Argument Exception: 1 argument must be provided"
+    exit 1
 fi
 
-counter=0
-trash_name="$filename-$counter"
-while [[ -e "$TRASH_DIR/$trash_name" ]] ; do
-    ((counter=counter + 1))
-    trash_name="$filename-$counter"
+if [ ! -d $trash ]; then
+    mkdir $trash
+fi
+
+if [ ! -f $trashlog ]; then
+    touch $trashlog
+fi
+
+name="$version"
+while [ -f $trash/$trashname ]; do
+    (( version++ ))
+    name="$version"
 done
 
-ln $filename "$TRASH_DIR/$trash_name"
-rm $filename && echo "$(realpath $filename) $trash_name" >> "$TRASH_LOG"
+ln -- "$filename" "$trash/$name"
+rm -- "$filename" && echo $(realpath -- "$filename") "$name" >> $trashlog
